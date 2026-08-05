7fabb69d4faeca2f8f4cd015eb231052c49c5001
Anjali-Pakhare <pakhareanjali47@gmail.com>
2026-08-05 10:11:04 +0530

    done

---
+++ sub.py
@@ -0,0 +1,9 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Sub the numbers
+sum = num1 - num2
+
+# Display the result
+print("The sub is:", sum)
